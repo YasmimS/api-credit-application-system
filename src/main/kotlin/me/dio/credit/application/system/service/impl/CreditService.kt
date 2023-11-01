20c0b8d6f9016235a39ab6d743db874df9ca4091
YasmimS <yasmim.hpforever@gmail.com>
2023-10-31 23:35:54 -0300

    Updated: credit limit and swagger document inserted

--- src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -5,12 +5,14 @@ import me.dio.credit.application.system.exception.BusinessException
 import me.dio.credit.application.system.repository.CreditRepository
 import me.dio.credit.application.system.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
 class CreditService(private val creditRepository: CreditRepository,
                     private val customerService: CustomerService): ICreditService {
     override fun save(credit: Credit): Credit {
+        this.validDayFirstInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -24,5 +26,10 @@ class CreditService(private val creditRepository: CreditRepository,
         val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
                 ?: throw BusinessException("Creditcode $creditCode not found"))
         return if (credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Invalid Date")
     }
 }
